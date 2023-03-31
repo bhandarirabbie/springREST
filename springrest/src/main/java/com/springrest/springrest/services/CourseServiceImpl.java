5d7bdcc903dba7a24922380f36b5f9eebbf74a5f
bhandarirabbie <me.rabinbhandari@gmail.com>
2023-04-01 00:47:44 +0900

    spring rest

---
+++ springrest/src/main/java/com/springrest/springrest/services/CourseServiceImpl.java
@@ -0,0 +1,50 @@
+package com.springrest.springrest.services;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.springrest.springrest.entities.Courses;
+
+@Service
+public class CourseServiceImpl implements CourseService {
+
+	List<Courses> list;
+	
+	public CourseServiceImpl() {
+		
+		list =new ArrayList<Courses>();
+		list.add(new Courses(15, "spring course", "this is spring"));
+		list.add(new Courses(145, "java course", "this is basis"));
+	}
+	
+	
+	@Override
+	public List<Courses> getCourses() {
+		return list;
+	}
+
+
+	@Override
+	public Courses getCourses(long courseId) {
+		
+		Courses c = null;
+		
+		for(Courses course:list) {
+			if(course.getId()==courseId) {
+				c=course;
+				break;
+			}
+		}
+		return c;
+	}
+
+
+	@Override
+	public Courses addCourse(Courses course) {
+		list.add(course);
+		return course;
+	}
+
+}
